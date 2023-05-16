f18a6d34829a40d934eb9b9b5939633c52f3bdaf
MinhVu11 <95182003+MinhVu11@users.noreply.github.com>
2023-05-16 09:00:12 +0700

    2ndUploadProject
    16/05/2023
    Change form's name

---
+++ ManageProjectApp/ManageProjectApp/FCreateNewSpace1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ManageProjectApp
+{
+    public partial class FCreateNewSpace1 : Form
+    {
+        public FCreateNewSpace1()
+        {
+            InitializeComponent();
+        }
+        private void buttonNext1_Click(object sender, EventArgs e)
+        {
+            FCreateNewSpace2 f2 = new FCreateNewSpace2();
+            f2.ShowDialog();
+            this.Close();
+        }
+    }
+}
---
+++ ManageProjectApp/ManageProjectApp/FCreateNewSpace2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ManageProjectApp
+{
+    public partial class FCreateNewSpace2 : Form
+    {
+        public FCreateNewSpace2()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonNext_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
